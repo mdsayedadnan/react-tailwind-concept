7fd45348acb49528e99834b7ff2a64d93d113e89
mdsayedadnan <sayedadnan6441@gmail.com>
2024-10-28 21:31:26 +0600

    first commit

---
+++ code-avater/src/App.jsx
@@ -0,0 +1,32 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+
+      <h1 className='border bg-slate-500 rounded-xl p-5 rounded-full mt-10 mb-10'>Vite + React</h1>
+      <div className="card card-side bg-base-100 shadow-xl">
+        <figure>
+          <img
+            src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp"
+            alt="Movie" />
+        </figure>
+        <div className="card-body">
+          <h2 className="card-title">New movie is released!</h2>
+          <p>Click the button to watch on Jetflix app.</p>
+          <div className="card-actions justify-end">
+            <button className="btn btn-primary">Watch</button>
+          </div>
+        </div>
+      </div>
+
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
